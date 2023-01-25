7d25cb7cb86c7f6c0379a74f2f1023303f7874cf
vladislav149 <vladislav149@mail.ru>
2023-01-25 21:32:31 +0300

    added some animation

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,13 @@
 <template>
-  <div @click="currentCard++">
-    <div v-if="currentCard < cards.length">
-      <div>Игрок - {{ Math.round((currentCard + 1) / 2) }}</div>
-      <div>{{ cards[currentCard] }}</div>
+  <transition name="game" appear mode="out-in">
+    <div @click="currentCard++" class="game" :key="currentCard">
+      <div v-if="currentCard < cards.length">
+        <div class="t-a-center">Игрок - {{ numberPlayer }}</div>
+        <div class="t-a-center">{{ cards[currentCard] }}</div>
+      </div>
+      <div v-else>время пошло...</div>
     </div>
-    <div v-else>время пошло...</div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -15,7 +17,7 @@ export default {
   name: 'SpyGame',
   data() {
     return {
-      textBetweenPlayers: 'Намите на экран',
+      textBetweenPlayers: 'Нажмите на экран',
       place: 'метро',
       currentCard: 0
     }
@@ -24,6 +26,9 @@ export default {
     ...mapGetters('players', ['getCardArr']),
     shuffleCard() {
       return shuffleArr(this.getCardArr(this.place))
+    },
+    numberPlayer() {
+      return Math.round((this.currentCard + 1) / 2)
     },
     cards() {
       const arr = []
@@ -38,4 +43,52 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.game {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: calc(100vh - 65px);
+  position: relative;
+}
+
+.game-enter-active {
+  animation: gameIn 1s;
+}
+
+.game-leave-active {
+  animation: gameOut 0.5s;
+}
+
+@keyframes gameIn {
+  0% {
+    transform: translateY(-100%);
+  }
+  60% {
+    transform: translateY(0);
+  }
+  75% {
+    transform: translateY(-15%);
+  }
+  90% {
+    transform: translateY(0);
+  }
+  95% {
+    transform: translateY(-5%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@keyframes gameOut {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+</style>
